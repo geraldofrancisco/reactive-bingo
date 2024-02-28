c07210ffee3d7fcbf17c156d2d022e79e05a5372
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2024-02-28 20:03:14 -0300

    rename class

--- src/main/java/com/geraldo/reactivebingo/domain/model/dto/PageDTO.java
+++ src/main/java/com/geraldo/reactivebingo/domain/model/dto/PageDTO.java
@@ -5,12 +5,12 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public record PageResponseDTO<T>(
+public record PageDTO<T>(
         List<T> response,
         Pageable pageable,
         Long total
 ) {
     @Builder(toBuilder = true)
-    public PageResponseDTO {
+    public PageDTO {
     }
 }
