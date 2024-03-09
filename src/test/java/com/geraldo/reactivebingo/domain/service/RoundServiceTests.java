e8178be971cf6324240b033244b735f51cee78c4
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2024-03-09 18:31:52 -0300

    finish unit tests

--- src/test/java/com/geraldo/reactivebingo/domain/service/RoundServiceTests.java
+++ src/test/java/com/geraldo/reactivebingo/domain/service/RoundServiceTests.java
@@ -5,8 +5,10 @@ import com.geraldo.reactivebingo.domain.execute.DrawExecutor;
 import com.geraldo.reactivebingo.domain.execute.GenerateCardExecutor;
 import com.geraldo.reactivebingo.domain.mapper.RoundMapper;
 import com.geraldo.reactivebingo.domain.model.document.round.RoundDocument;
+import com.geraldo.reactivebingo.domain.model.dto.player.Player;
 import com.geraldo.reactivebingo.domain.model.dto.round.Round;
 import com.geraldo.reactivebingo.domain.model.dto.round.RoundCard;
+import com.geraldo.reactivebingo.domain.model.dto.round.RoundPlayer;
 import com.geraldo.reactivebingo.domain.validate.GenerateCardValidate;
 import com.geraldo.reactivebingo.repository.RoundRepository;
 import org.bson.types.ObjectId;
@@ -25,8 +27,11 @@ import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
+import static com.geraldo.reactivebingo.domain.constants.ErrorMessages.ROUND_CREATE_CARD_EXISTS_CARD_FOR_PLAYER;
+import static com.geraldo.reactivebingo.domain.constants.ErrorMessages.ROUND_CREATE_CARD_INVALID_STATUS;
 import static com.geraldo.reactivebingo.domain.constants.ErrorMessages.ROUND_LAST_NUMBER_DRAWN_NOT_RUNNING;
 import static com.geraldo.reactivebingo.domain.constants.ErrorMessages.ROUND_NOT_FOUND;
 import static com.geraldo.reactivebingo.domain.constants.ErrorMessages.ROUND_NUMBER_OF_INVALID_CARDS_FOR_THE_ROUND;
@@ -36,6 +41,7 @@ import static com.geraldo.reactivebingo.domain.model.enums.RoundStatus.FINISHED;
 import static com.geraldo.reactivebingo.domain.model.enums.RoundStatus.RUNNING;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
@@ -65,7 +71,9 @@ public class RoundServiceTests {
 
     @Before
     public void init() {
-        setField(drawExecutor, "random", new Random());
+        var random = new Random();
+        setField(drawExecutor, "random", random);
+        setField(generateCardExecutor, "random", random);
     }
 
     @Test
@@ -224,7 +232,76 @@ public class RoundServiceTests {
 
     @Test
     public void generateCardSuccessTest() {
+        var round = getRound().status(CREATED).build();
+        var player = getPlayer().build();
 
+        getByIdSuccessTraining(round);
+        playerGetByIdTraining(player);
+        when(generateCardValidate.validate(any(), any()))
+                .thenCallRealMethod();
+        when(generateCardExecutor.execute(any()))
+                .thenCallRealMethod();
+        saveTraining(round);
+
+        var result = service.generateCard(ID_EXAMPLE, ID_EXAMPLE);
+
+        StepVerifier.create(result)
+            .expectNextMatches(Objects::nonNull)
+            .verifyComplete();
+    }
+
+    @Test
+    public void generateCardRoundInvalidStatusErrorTest() {
+        var round = getRound().status(RUNNING).build();
+        var player = getPlayer().build();
+
+        getByIdSuccessTraining(round);
+        playerGetByIdTraining(player);
+        when(generateCardValidate.validate(any(), any()))
+                .thenCallRealMethod();
+
+        var result = service.generateCard(ID_EXAMPLE, ID_EXAMPLE);
+
+        StepVerifier.create(result)
+            .expectErrorMessage(ROUND_CREATE_CARD_INVALID_STATUS)
+            .verify();
+    }
+
+    @Test
+    public void generateCardRoundExistsPlayerErrorTest() {
+        var player = getPlayer()
+                .id(ID_EXAMPLE)
+                .build();
+
+        var cards = List.of(getRoundCard()
+                .owner(getRoundPlayer())
+                .build());
+
+        var round = getRound()
+                .status(CREATED)
+                .cards(cards)
+                .build();
+
+        getByIdSuccessTraining(round);
+        playerGetByIdTraining(player);
+        when(generateCardValidate.validate(any(), any()))
+                .thenCallRealMethod();
+
+        var result = service.generateCard(ID_EXAMPLE, ID_EXAMPLE);
+
+        StepVerifier.create(result)
+                .expectErrorMessage(ROUND_CREATE_CARD_EXISTS_CARD_FOR_PLAYER)
+                .verify();
+    }
+
+    private RoundPlayer getRoundPlayer() {
+        return RoundPlayer.builder()
+                .id(ID_EXAMPLE)
+                .build();
+    }
+
+    private Player.PlayerBuilder getPlayer() {
+        return Player.builder();
     }
 
     private RoundCard.RoundCardBuilder getRoundCard() {
@@ -240,6 +317,11 @@ public class RoundServiceTests {
         when(mapper.toDocument(any())).thenReturn(getRoundDocument());
         when(repository.save(any())).thenReturn(Mono.just(getRoundDocument()));
         when(mapper.toRound(any())).thenReturn(round);
+    }
+
+    private void playerGetByIdTraining(Player player) {
+        when(playerService.getById(anyString()))
+                .thenReturn(Mono.just(player));
     }
 
     private void getByIdSuccessTraining(Round round) {
